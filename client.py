a52fde0a8e13ef7d24f0c343df832f342ad1e84e
CK Thang <ckcangmah@gmail.com>
2026-04-16 18:19:05 -0400

    finalized client and server files

--- client.py
+++ client.py
@@ -1,13 +1,41 @@
 from socket import socket, AF_INET, SOCK_STREAM #import socket class and constants 
+import threading 
 
+#client receiving message, continuously prints it unless quit 
+#use try/except to ensure smooth quit without receiver throwing error once socket closes
+def receive(client_sock):
+    while(True):
+        try: 
+            resp = client_sock.recv(1024) #receives 1KB of data from server
+            if (resp == b''):
+                client_sock.close()
+                break #if quit, stop receiving exit loop
+            print("Server message: ", resp.decode())
+        except:
+            break
+#client sends message to server continuously
+def send(client_sock):
+    while(True):
+        message = input("Input Message: ")
+        if(message == 'quit' or message == ''):
+            client_sock.close()
+            break #exit loop if quit 
+        try: #avoid error if socket has closed from server side using try/except, instead break cleanly
+            client_sock.send(message.encode()) #send encoded message to server
+        except:
+            break
+
+#creating client socket and connect to server
 client_sock = socket(AF_INET, SOCK_STREAM) #create client's socket object 
 client_sock.connect(('localhost', 50000)) #establish a connection to server socket 
-resp = client_sock.recv(1024) #receives message from server 
-print("Server says: ", resp.decode()) #print server's message decoding the byte to string
 
-#sending a response message to server
-message = input("Input message: ") #get input from user
-client_sock.send(message.encode()) #send the input as encoded string to byte 
+#create threads and start for both functions
+t1 = threading.Thread(target=receive, args=(client_sock,))
+t2 = threading.Thread(target=send, args=(client_sock,))
 
-#close the socket
-client_sock.close()
+#start threads
+t1.start()
+t2.start()
+#end main after threads ends
+t1.join()
+t2.join()
--- server.py
+++ server.py
@@ -7,18 +7,26 @@ def send(csock):
     while (True): #continuously sends user's input to client until user 'quit' the connection
         message = input("Enter message: ") #prompts user for input
         if (message == "quit"):
+            csock.close()
             break
-        csock.send(message.encode()) #send the message via client socket in bytes via .encode()
-
+        try: #use try/except incase client has closed sockets
+            csock.send(message.encode()) #send the message via client socket in bytes via .encode()
+        except:
+            break
 #receiving client's message to server on client socket
+#use try/except to ensure no error thrown by receiver once socket is closed
 def receive(csock):
     while(True):
-        resp = csock.recv(1024) #receives up to 1KB of data/message from client
-        if (resp == b"quit" or resp == b''): #if user input is quit or empty, end 
-            break 
-        print("Client message: ", resp.decode()) #print the message after decoding the bytes 
+        try: 
+            resp = csock.recv(1024) #receives up to 1KB of data/message from client
+            if (resp == b''): #if user input is quit or empty, end 
+                break 
+            print("Client message: ", resp.decode()) #print the message after decoding the bytes 
+        except:
+            break
 
-server_sock = socket(AF_INET, SOCK_STREAM) #create a socket object using imported socket class
+#create a socket object using imported socket class
+server_sock = socket(AF_INET, SOCK_STREAM) 
 server_sock.bind(('localhost', 50000)) #bind/attaching the socket object to local machine on port 50000
 server_sock.listen(1) #socket is open for a single connection 
 
@@ -30,14 +38,14 @@ print("Connection successful with: ", caddr) #prints the address/port of client
 
 #creating two threads for sending and receiving messages to client 
 #target is the function name and args is the function argument as tuple
-t1 = threading.Thread(target=send, args=(csock,)) 
+t1 = threading.Thread(target=send, args=(csock,))
 t2 = threading.Thread(target=receive, args=(csock,))
+
 #start threads 
 t1.start()
 t2.start()
 #main waits for threads to exit before closing sockets 
 t1.join()
 t2.join()
-#close the sockets 
-csock.close()
+#close the socket 
 server_sock.close()
